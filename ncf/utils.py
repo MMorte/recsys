eb2802d84e191b661bd82c37dc3fc5cae8db91f8
Matousek <martin.matousek@outlook.com>
2020-04-18 14:20:03 +0200

    fastai utils

---
+++ ncf/utils.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+
+def ifnone(a: Any, b: Any) -> Any:
+    """'a' if 'a' is not None, otherwise 'b'.
+    
+    Parameters
+    ----------
+    a : Any
+    b : Any
+    
+    Returns
+    -------
+    Any
+        'a' or 'b' if a is None
+    """
+    return b if a is None else a
+
+
+def trunc_normal_(x: Tensor, mean: float = 0.0, std: float = 1.0) -> Tensor:
+    "Truncated normal initialization."
+    # From https://discuss.pytorch.org/t/implementing-truncated-normal-initializer/4778/12
+    return x.normal_().fmod_(2).mul_(std).add_(mean)
+
+
+def embedding(ni: int, nf: int) -> nn.Module:
+    "Create an embedding layer."
+    emb = nn.Embedding(ni, nf)
+    # See https://arxiv.org/abs/1711.09160
+    with torch.no_grad():
+        trunc_normal_(emb.weight, std=0.01)
+    return emb
