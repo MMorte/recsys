04e24e54c3172e3c269078a717f602d92d4bb9a6
Matousek <martin.matousek@outlook.com>
2020-04-19 17:20:37 +0200

    add RMSELoss function, remove ifnone, move trunc_normal_ into Embedding, add docstrings

--- ncf/model/_utils.py
+++ ncf/model/_utils.py
@@ -3,32 +3,53 @@ import torch.nn as nn
 from typing import Any
 
 
-def ifnone(a: Any, b: Any) -> Any:
-    """'a' if 'a' is not None, otherwise 'b'.
+def Embedding(ni: int, nf: int) -> nn.Embedding:
+    """Create an embedding layer (shape ni x nf) using truncated normal initialization.
+    See: https://arxiv.org/abs/1711.09160 [Embeddings]
+         https://discuss.pytorch.org/t/implementing-truncated-normal-initializer/4778/12 [Initialization]
     
     Parameters
     ----------
-    a : Any
-    b : Any
+    ni : int
+        Number of inputs
+    nf : int
+        Number of factors
     
     Returns
     -------
-    Any
-        'a' or 'b' if a is None
+    nn.Embedding
+        
     """
-    return b if a is None else a
-
-
-def trunc_normal_(x: torch.tensor, mean: float = 0.0, std: float = 1.0) -> torch.tensor:
-    "Truncated normal initialization."
-    # From https://discuss.pytorch.org/t/implementing-truncated-normal-initializer/4778/12
-    return x.normal_().fmod_(2).mul_(std).add_(mean)
-
-
-def embedding(ni: int, nf: int) -> nn.Module:
-    "Create an embedding layer."
+    # Embedding
     emb = nn.Embedding(ni, nf)
-    # See https://arxiv.org/abs/1711.09160
+    # Initialization
+    std = 0.01
+    mean = 0
     with torch.no_grad():
-        trunc_normal_(emb.weight, std=0.01)
+        # as if "inplace=True"
+        emb.weight.normal_().fmod_(2).mul_(std).add_(mean)
     return emb
+
+
+class RMSELoss(nn.Module):
+    """RMSELoss with eps to avoid tensor([nan]) during backprop.
+    See: https://discuss.pytorch.org/t/rmse-loss-function/16540/3
+
+    Parameters
+    ----------
+    eps : float
+        Small num to avoid nan, defaults to 1e-6
+    
+    Returns
+    -------
+    loss : float
+    """
+
+    def __init__(self, eps=1e-20):
+        super().__init__()
+        self.mse = nn.MSELoss()
+        self.eps = eps
+
+    def forward(self, yhat, y):
+        loss = torch.sqrt(self.mse(yhat, y) + self.eps)
+        return loss
